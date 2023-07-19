c6d29b0efea5c26319da73a3ea58e0fba96209fe
Daniel-Shapira Laptop <danielshapira3@gmail.com>
2023-07-19 13:41:02 +0300

    Started working on the Music Blog

--- src/containers/musicblog/Musicblog.jsx
+++ src/containers/musicblog/Musicblog.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './musicblog.css'
+import MusicLogo1 from "../../assets/"
 
 const Musicblog = () => {
   return (
-    <div>Musicblog</div>
+    <div className='TextSound'>
+      <h2 className='HeadLineText'>Authentic Music, Meaningful Influence<p className='centralText'>Collaborating with top music creators worldwide, we curate exclusive and authentic new tracks daily, just for you.</p></h2>
+
+      <div className='MusicCatalog'>
+        <div className='MusicGenre1'>
+          <div>
+            <img></img>
+            <a>Electronic Dance</a>
+          </div>
+        </div>
+        <div className='MusicGenre2'>
+          <div>
+            <img></img>
+            <a>Pop</a>
+          </div>
+        </div>
+        <div className='MusicGenre3'>
+          <div>
+            <img></img>
+            <a>Rap</a>
+          </div>
+        </div>
+        <div className='MusicGenre4'>
+          <div>
+            <img></img>
+            <a>Lofi</a>
+          </div>
+        </div>
+        <div className='MusicGenre5'>
+          <div>
+            <img></img>
+            <a>Indie</a>
+          </div>
+        </div>
+        <div className='MusicGenre6'>
+          <div>
+            <img></img>
+            <a>Trap</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
