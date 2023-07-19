eb864314e5c63851587a6178a8eb71bc2965df8c
Daniel Shapira <116350669+DanielShapi4a@users.noreply.github.com>
2023-07-19 12:20:00 +0300

    added grid box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { Footer, Blog, Possibility, Features, Musicblog, Header } from './containers';
 import {CTA,Brand,Navbar} from './components';
 import {useState, useEffect} from 'react';
+import Logo1 from "../src/assets/HyperXLogo.png"
+import Logo2 from "../src/assets/JBL-Logo.png"
+import Logo3 from "../src/assets/Logitech_logo_white.svg"
+import Logo4 from "../src/assets/RazerLogo.png"
+import Logo5 from "../src/assets/SennheiserLogo.jpeg"
+import Logo6 from "../src/assets/SteelSeriesLogo.png"
+import Logo7 from "../src/assets/jabra-logo.png"
+import Logo8 from "../src/assets/samsung-logo.png"
 
 function App() {
   return (
@@ -12,11 +20,20 @@ function App() {
         <Header/>
       </div>
       <Brand/>
-      <div>
-        <div className='Sponsors-div'></div>
-        <div className='Sponsors-div'></div>
+      <div className='Central-sponsor'>
+        <div className='FirstRow'>
+          <img src={Logo1} />
+          <img src={Logo2} />
+          <img src={Logo3} />
+          <img src={Logo4} />
+        </div>
+        <div className='SecondRow'>
+          <img src={Logo5} />
+          <img src={Logo6} />
+          <img src={Logo7} />
+          <img src={Logo8} />
+        </div>
       </div>
-      
 
       <Musicblog/>
       <Features/>
