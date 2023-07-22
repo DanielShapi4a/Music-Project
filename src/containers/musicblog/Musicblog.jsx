703a08b367678117cccaac2e6e916cf20d674bef
Daniel-Shapira Laptop <danielshapira3@gmail.com>
2023-07-22 19:06:45 +0300

    Finished working on the music Genres!

--- src/containers/musicblog/Musicblog.jsx
+++ src/containers/musicblog/Musicblog.jsx
@@ -8,43 +8,60 @@ import MusicLogo5 from "../../assets/Trap.jpg"
 import MusicLogo6 from "../../assets/Rap.jpg"
 
 
+
+import { useState, useRef } from 'react';
+
 const Musicblog = () => {
+  const [currentGenre, setCurrentGenre] = useState(null);
+  const audioRef = useRef(null);
+
+  const playMusic = (genre, startTime) => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = startTime;
+    }
+    const audioElement = new Audio(`${process.env.PUBLIC_URL}/music-samples/${genre}.mp3`);
+    audioElement.currentTime = startTime;
+    audioElement.play();
+    setCurrentGenre(genre);
+    audioRef.current = audioElement;
+  };
+
   return (
-    <div >
+    <div>
       <div className='TextSound'>
         <h2 className='HeadLineText'>Authentic Music, Meaningful Influence<p className='centralText'>Collaborating with top music creators worldwide, we create exclusive and authentic new tracks daily, just for you.</p></h2>
+        <p>Try one of our many genre &#x1F604;</p>
       </div>
-
 
       <div className='MusicCatalog'>
         <a className='MusicGenre1'>
-          <img src={MusicLogo1} alt='Lofi' />
+          <img src={MusicLogo1} alt='Lofi' onClick={() => playMusic('Lofi', 12)}/>
           Lofi
         </a>
         <a className='MusicGenre2'>
-          <img src={MusicLogo2} alt='Electronic Dance' />
+          <img src={MusicLogo2} alt='Electronic Dance' onClick={() => playMusic('Electronic Dance', 3)}/>
           Electronic Dance
         </a>
         <a className='MusicGenre3'>
-          <img src={MusicLogo3} alt='Pop' />
+          <img src={MusicLogo3} alt='Pop' onClick={() => playMusic('Pop', 0)}/>
           Pop
         </a>
         <a className='MusicGenre4'>
-          <img src={MusicLogo4} alt='Indie' />
+          <img src={MusicLogo4} alt='Indie' onClick={() => playMusic('Indie', 0)} />
           Indie
         </a>
         <a className='MusicGenre5'>
-          <img src={MusicLogo5} alt='Trap' />
+          <img src={MusicLogo5} alt='Trap' onClick={() => playMusic('Trap', 46)}/>
           Trap
         </a>
         <a className='MusicGenre6'>
-          <img src={MusicLogo6} alt='Rap' />
+          <img src={MusicLogo6} alt='Rap' onClick={() => playMusic('Rap', 89)}/>
           Rap
         </a>
       </div>
-
     </div>
   )
 }
 
-export default Musicblog
+export default Musicblog;
