cb2570a5d97a8d5594e27c4b193962c85edc5bdd
Daniel Shapira <danielshapira3@gmail.com>
2023-08-18 22:41:39 +0300

    Added a hardCode on fireBase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,13 +6,15 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMRK5cakjbtnMymWLwi26gFcxyP8r_AKk",
-  authDomain: "music-project-4d9b1.firebaseapp.com",
-  projectId: "music-project-4d9b1",
-  storageBucket: "music-project-4d9b1.appspot.com",
-  messagingSenderId: "257187841887",
-  appId: "1:257187841887:web:82d3a599314bdb630418ea"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
